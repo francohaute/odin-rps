28954789e8cf2f26d95d552eafe4c74989de6c2e
Franco Hauteville <francohauteville@gmail.com>
2025-01-14 14:44:19 -0300

    UI added

--- app.js
+++ app.js
@@ -16,15 +16,6 @@ function isChoiceValid(choice) {
   return choice == "rock" || choice == "paper" || choice == "scissors";
 }
 
-function getUserChoice() {
-  let choice = prompt("Select: rock, paper or scissors").toLowerCase();
-  if (!isChoiceValid(choice)) {
-    choice = getUserChoice();
-  } else {
-    return choice;
-  }
-}
-
 function getResult(userChoice, computerChoice) {
   switch (true) {
     case userChoice == computerChoice:
@@ -45,39 +36,56 @@ function getResult(userChoice, computerChoice) {
   }
 }
 
-function playGame() {
-  let userScore = 0;
-  let computerScore = 0;
-  let ties = 0;
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+rockButton.addEventListener("click", () => {
+  playRound("rock");
+});
+paperButton.addEventListener("click", () => {
+  playRound("paper");
+});
+scissorsButton.addEventListener("click", () => {
+  playRound("scissors");
+});
 
-  while (userScore < 3 && computerScore < 3) {
-    let userChoice = getUserChoice();
-    let computerChoice = getComputerChoice();
-    let result = getResult(userChoice, computerChoice);
-    if (result == "tie") {
+const resultBoard = document.querySelector("#result");
+const winnerBoard = document.querySelector("#winner");
+
+let userScore = 0;
+let computerScore = 0;
+let ties = 0;
+
+function clearBoard() {
+  userScore = 0;
+  computerScore = 0;
+  ties = 0;
+}
+
+function updateScore(result) {
+  switch (result) {
+    case "tie":
       ties++;
-    } else if (result == "computer") {
+      break;
+    case "computer":
       computerScore++;
-    } else {
+      break;
+    case "user":
       userScore++;
-    }
-    console.log(
-      "User: " +
-        userScore +
-        " | Computer: " +
-        computerScore +
-        " | Ties: " +
-        ties,
-    );
+      break;
   }
-  console.log("User: " + userScore);
-  console.log("Computer: " + computerScore);
-  console.log("Ties: " + ties);
+  resultBoard.textContent = `Results: User ${userScore} | Computer ${computerScore} | Ties: ${ties}`;
   if (userScore == 3) {
-    console.log("User WON!!!");
-  } else {
-    console.log("Computer won");
+    winnerBoard.textContent = "The User WINS!!!";
+    clearBoard();
+  } else if (computerScore == 3) {
+    winnerBoard.textContent = "The Computer WINS!!!";
+    clearBoard();
   }
 }
 
-playGame();
+function playRound(userChoiceSelected) {
+  let computerChoice = getComputerChoice();
+  let roundResult = getResult(userChoiceSelected, computerChoice);
+  updateScore(roundResult);
+}
